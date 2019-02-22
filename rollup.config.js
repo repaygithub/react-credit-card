e56e7a5b92e1f3b2c693faa3fa63069b3cddb036
JamesNimlos <jnimlos@repay.com>
2019-02-22 13:25:24 -0700

    chore(build): setup rollup build

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import cleanup from 'rollup-plugin-cleanup'
+import filesize from 'rollup-plugin-filesize'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import pkg from './package.json'
+
+const input = 'src/Card.tsx'
+const peerDeps = Object.keys(pkg.peerDependencies)
+
+export default [
+  {
+    input,
+    external: id => peerDeps.includes(id),
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve({
+        extensions: ['.js', 'jsx', '.ts', '.tsx'],
+      }),
+      babel({
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.es6', '.es', '.mjs'],
+      }),
+      cleanup(),
+      sourceMaps(),
+      filesize(),
+    ],
+  },
+]
